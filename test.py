0b0ef48570eeeaaf47d0cb99bf71d841698080c7
alandantasmoura <alandantasmoura@gmail.com>
2018-12-03 13:08:06 -0500

    Alteracao no DOM index.html

--- test.py
+++ test.py
@@ -19,7 +19,7 @@ class Test(unittest.TestCase):
         result = self.app.get('/') 
 
         # verifica o retorno do conteudo da pagina
-        self.assertRegex(result.data.decode(), "testando1234")
+        self.assertRegex(result.data.decode(), "Meu primeiro deploy - DEVOPS")
 
 
 if __name__ == "__main__":
